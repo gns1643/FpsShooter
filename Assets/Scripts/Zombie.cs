a30cb870c9aba5af3d320d06abd512ffb1e0514b
gns1643 <gns1643@naver.com>
2025-07-23 20:54:59 +0900

    feat: 좀비 걷고 뛰기 추가
    플레이어 오브젝트와의 거리에 따라서 걷고 뜀

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -3,12 +3,24 @@ using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
+
     [Header("필요한 컴포넌트들")]
-    [SerializeField] Transform thePlayer;
-    [SerializeField] Animator anim;
-    NavMeshAgent nav;
+    [SerializeField] private Transform thePlayer;
+    [SerializeField] private Animator anim;
+    private NavMeshAgent nav;
+
+    [Header("좀비의 스탯")]
+    [SerializeField] int maxHp;
+    private int currentHp;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float distanceToRun;
+
+
+
     void Start()
     {
+        currentHp = maxHp;
         nav = gameObject.GetComponent<NavMeshAgent>(); 
     }
 
@@ -23,12 +35,27 @@ public class Zombie : MonoBehaviour
         if (nav != null)
         {
             nav.SetDestination(thePlayer.position);
-          
-            anim.SetBool("Running", true); //기능 추가 필요 : 플레이어와의 거리에따라 겉기, 뛰기 전환
+
+            //플레이어와의 거리에따라 겉기, 뛰기 전환
+            float distance = Vector3.Distance(thePlayer.position, transform.position);
+            if (distance >= distanceToRun)
+                Running();
+            else
+                Walking();
         }
         else
         {
             Debug.Log("좀비가 쫓을 목표가 없음");
         }
+    }
+    void Running()
+    {
+        anim.SetBool("Running", true);
+        nav.speed = runSpeed;
+    }
+    void Walking()
+    {
+        anim.SetBool("Running", false);
+        nav.speed = walkSpeed;
     }
 }
