23916c820369fef0812d9994e19579edf0ae1c68
gns1643 <gns1643@naver.com>
2025-08-10 16:39:23 +0900

    feat: 거미줄 함정 오류 메시지 수정

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -3,12 +3,8 @@ using UnityEngine;
 
 public class HealthUI : MonoBehaviour
 {
-    private TMP_Text hp_Text;
+    public TMP_Text hp_Text;
     public PlayerStatus thePlayerStatus;
-    private void Start()
-    {
-        hp_Text = GetComponentInChildren<TMP_Text>();
-    }
     public void HpUpdate()
     {
         hp_Text.text = thePlayerStatus.currentHp.ToString();
