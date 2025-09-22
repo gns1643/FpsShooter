14f8993b74488a2b1d337e6274aa169232482b74
gns1643 <gns1643@naver.com>
2025-09-22 17:27:37 +0900

    feat: 게임 플레이 방식 소개 추가

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -3,18 +3,42 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    public GameObject howToPlayPanel; 
+    public GameObject howToPlay1;     
+    public GameObject howToPlay2;
+    public int currentStats = 0;
+
     private void Start()
     {
         SoundManager.instance.PlayBGM("TitleScene");
+        currentStats = 0;
     }
 
     public void ClickStart(string sceneName)
     {
-        SoundManager.instance.PlaySE("ButtonClick");
-        SoundManager.instance.StopBGM();
-        SceneManager.LoadScene(sceneName);
+        howToPlayPanel.SetActive(true);
+        howToPlay1.SetActive(true);
+        howToPlay2.SetActive(false);
     }
 
+    public void OnClickNext(string sceneName)
+    {
+        SoundManager.instance.PlaySE("ButtonClick");
+
+        if (currentStats == 0)
+        {
+            howToPlay1.SetActive(false);
+            howToPlay2.SetActive(true);
+            currentStats++;
+        }
+        else if (currentStats == 1)
+        {
+            howToPlayPanel.SetActive(false);
+            SoundManager.instance.StopBGM();
+            SceneManager.LoadScene(sceneName);
+            currentStats = 0;
+        }
+    }
 
     public void ClickExit()
     {
